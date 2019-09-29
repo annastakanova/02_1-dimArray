46e7d940c6d44852bc3fd942becb1eb085311426
a.stakanova <stakanova@gmail.com>
2019-09-30 00:41:12 +0300

    task 17-20 alfa

--- Task16_1dim.java
+++ Task16_1dim.java
@@ -2,18 +2,20 @@
 //max(a1+a(2n), a2+a(2n−1),..., an+a(n+1)).
 
 
+//не поняла условие
 
 package com.epam;
 
 public class Task16_1dim {
     public static void main(String[] args) {
-        double[] array = {0.2, 2.6, 4.0, 0.8, 7.0, 0.0, 6.6};
+        double[] array = {0.2, 2.6, 4.0, 9.8, 7.0, 0.0, 2.0};
         int n = array.length;
-        double maxResult = array[0]+array[2*n];
+        double maxResult = array[0] + 2*array[n];
+        System.out.println(maxResult);
 
-        for(int i = 0; i < array.length; i++) {
+        for(int i = 0; i<n; i++) {
             if (array[i]+array[2*n-i+1] > maxResult)
-                maxResult = array[i] + array[2*n-i+1];
+                maxResult = array[i] + 2*array[n-i+1];
             System.out.println(maxResult);
         }
     }
